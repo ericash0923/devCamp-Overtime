d0f470d4fac745811fce48030fc9ad5c6130d584
ericash0923 <eric@gaglani.com>
2017-11-25 03:08:10 +0000

    user connection to post with tests

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -4,6 +4,7 @@ describe 'navigate' do
   describe 'homepage' do
     it 'can be reached successfully' do
       visit root_path
+      # visit new_user_session_path
       expect(page.status_code).to eq(200)
     end
   end
