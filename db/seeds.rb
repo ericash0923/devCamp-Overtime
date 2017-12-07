e535291989cba5df6d0d94e6c51fb186552b2f40
ericash0923 <eric@gaglani.com>
2017-12-07 04:31:16 +0000

    add enum to admin dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 
 puts "1 user created"
 
+AdminUser.create(email: "myadmin@admin.com", password: "asdfasdf", 
+                         password_confirmation: "asdfasdf",
+                         first_name: "Admin",
+                         last_name: "User")
+
+puts "1 admin created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
