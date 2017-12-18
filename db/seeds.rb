58c5da1d002092650a96ea04eb2dd4d61b3ff69f
ericash0923 <eric@gaglani.com>
2017-12-18 20:20:06 +0000

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+AdminUser.create(email: "myadmin@admin.com", password: "asdfasdf", 
+                         password_confirmation: "asdfasdf",
+                         first_name: "Admin",
+                         last_name: "User",
+                         phone: "3473300776")
+
+puts "1 admin created"
+
 @user = User.create(email: "test@test.com", password: "asdfasdf", 
                          password_confirmation: "asdfasdf",
                          first_name: "John",
@@ -6,13 +14,6 @@
 
 puts "1 user created"
 
-AdminUser.create(email: "myadmin@admin.com", password: "asdfasdf", 
-                         password_confirmation: "asdfasdf",
-                         first_name: "Admin",
-                         last_name: "User",
-                         phone: "3473300776")
-
-puts "1 admin created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
