75c083e5f9776643a63231df95f6ec568cab3a42
ericash0923 <eric@gaglani.com>
2017-12-20 01:45:55 +0000

    setup buttons for employee homepage for no overtime confirmations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :audit_logs, expect: [:new, :edit, :destory]
+  resources :audit_logs, expect: [:new, :edit, :destory] do
+    member do 
+      get :confirm
+    end
+  end
   
   namespace :admin do
     resources :users
@@ -14,6 +18,7 @@ Rails.application.routes.draw do
       get :approve
     end
   end
+  
   devise_for :users, skip: [:registrations]
   root to: 'static#homepage'
 end
