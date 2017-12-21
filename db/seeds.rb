4e9cdc58a1c65d5c80f888b56436c9a6c9a4a7b1
ericash0923 <eric@gaglani.com>
2017-12-21 02:49:13 +0000

    change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.
 
 puts "3 audit logs have been created"
 
-100.times do |post|
+101.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
               ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
